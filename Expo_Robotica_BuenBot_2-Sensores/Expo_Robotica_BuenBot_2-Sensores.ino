1a7ea304faf66783cd720b2ff0e3d59e3a61bfed
Claudio Ivan Morales Gutierrez <claudio.morales1401@alumnos.ubiobio.cl>
2017-09-25 02:02:02 -0300

    funcion avanzar modificada

--- Expo_Robotica_BuenBot_2-Sensores/Expo_Robotica_BuenBot_2-Sensores.ino
+++ Expo_Robotica_BuenBot_2-Sensores/Expo_Robotica_BuenBot_2-Sensores.ino
@@ -1,18 +1,20 @@
 /*
-  EVALUAR SI LEER ANTES LAS VARIABLES DE DATOS
+EVALUAR SI LEER ANTES LAS VARIABLES DE DATOS
+
+VER VUELTAS CORTAS DE GIRAR
 */
 
 /*
-  EXPO ROBOTICA UBB 2017
-  CLAUDIO MORALES GUTIERREZ
-  IECI
+EXPO ROBOTICA UBB 2017
+CLAUDIO MORALES GUTIERREZ
+IECI
 */
 int var_estado_ruedas;
 /*
 variable de estado 3 : 
-  0: esta a la izquierda
-  1 esta recto 
-  2 esta a la derecha
+0: esta a la izquierda
+1 esta recto 
+2 esta a la derecha
 Se inicializa en 1 porque parte recto
 */
 
@@ -59,61 +61,61 @@ void setup(){
 
   Serial.begin(9600);
   delay(5000);
-    calibrar(); // calibra poniendo en falso
-    pinMode(sen_izq,INPUT);   // sensor inicia como entrada
-    pinMode(sen_der,INPUT);   // snesor inicia como entrada
-  
-    /*MOTOR DELANTERO*/
-    pinMode(ENA,OUTPUT);
-    pinMode (IN1, OUTPUT);    // Input1 conectada al pin _ 
-    pinMode (IN2, OUTPUT);    // Input2 conectada al pin _ 
-  
-    /*MOTOR DELANTERO*/
-    pinMode(ENB,OUTPUT);
-    pinMode (IN3, OUTPUT);    // Input3 conectada al pin _ 
-    pinMode (IN4, OUTPUT);    // Input4 conectada al pin _
+  calibrar(); // calibra poniendo en falso
+  pinMode(sen_izq,INPUT);   // sensor inicia como entrada
+  pinMode(sen_der,INPUT);   // snesor inicia como entrada
+
+  /*MOTOR DELANTERO*/
+  pinMode(ENA,OUTPUT);
+  pinMode (IN1, OUTPUT);    // Input1 conectada al pin _ 
+  pinMode (IN2, OUTPUT);    // Input2 conectada al pin _ 
+
+  /*MOTOR DELANTERO*/
+  pinMode(ENB,OUTPUT);
+  pinMode (IN3, OUTPUT);    // Input3 conectada al pin _ 
+  pinMode (IN4, OUTPUT);    // Input4 conectada al pin _
 
 
 
-    byte sentido_giro=1;
+  byte sentido_giro=1;
 
-    var_est=1;
+  var_est=1;
 
-    line=100;
+  line=100;
 
 }
 
 void loop(){
-  //detecta la pista  
-  if((analogRead(sen_izq)>line) && (analogRead(sen_der)>line) ){ // si el sensor izquierdo y derecha detecta la linea blanca
-    //rueda directa 
-    avanzar();
-  }
+//detecta la pista  
+if((analogRead(sen_izq)>line) && (analogRead(sen_der)>line) ){ // si el sensor izquierdo y derecha detecta la linea blanca
+  //rueda directa 
+  avanzar();
+}
 
 
-  if((analogRead(sen_izq)<line) && (analogRead(sen_der)<line) ){// si el sensor izquierdo y derecha detecta la linea blanca
-    //ver el sentido de giro con un boton
-    if (sentido==1){
-      giro_der(1000,1000,1000);
+if((analogRead(sen_izq)<line) && (analogRead(sen_der)<line) ){// si el sensor izquierdo y derecha detecta la linea blanca
+  //ver el sentido de giro con un boton
+  if (sentido==1){
+    giro_der(1000,1000,1000);
     }else if(sentido==0){
       giro_izq(1000,1000,1000);
     }
+
+  }
+
+//Detecta el sensor de linea derecha      
+if((analogRead(sen_izq)>line)  && (analogRead(sen_der)<line)){ // si el sensor derecho detecta la linea blanca 
+  //Giro a la Izquierda, sera con sentidos?
+  
+}  
+
+
+// Detecta la linea I zquierda
+  if((analogRead(sen_izq)<line) && (analogRead(sen_der)>line) ){ // si el sensor izquierdo detecta la linea blanca
+    //Giro a la Derecha sentidos?
     
   }
 
-  //Detecta el sensor de linea derecha      
-  if((analogRead(sen_izq)>line)  && (analogRead(sen_der)<line)){ // si el sensor derecho detecta la linea blanca 
-    //Giro a la Izquierda, sera con sentidos?
-    izquierda();
-  }  
-
-
-  // Detecta la linea I zquierda
-    if((analogRead(sen_izq)<line) && (analogRead(sen_der)>line) ){ // si el sensor izquierdo detecta la linea blanca
-      //Giro a la Derecha sentidos?
-      derecha();
-    }
-  
 
 }
 
@@ -123,98 +125,107 @@ void loop(){
 //trdd : tiempo ruedas delanteras delante
 void giro_der(int trda,int trta,int trdd){
 
-    // motor delantero a un lado
-    analogWrite(ENA, velocidad_maxima);
-    digitalWrite (IN1, LOW);
-    digitalWrite (IN2, HIGH);
-    
-    // motor trasero atras
-    analogWrite(ENB, velocidad_maxima);
-    digitalWrite (IN3, HIGH);
-    digitalWrite (IN4, LOW);
-    delay(trda);
+  // motor delantero a un lado
+  analogWrite(ENA, velocidad_maxima);
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, HIGH);
+  
+  // motor trasero atras
+  analogWrite(ENB, velocidad_maxima);
+  digitalWrite (IN3, HIGH);
+  digitalWrite (IN4, LOW);
+  delay(trda);
 
-    //Dobla al sentido contrario hacia delante
-    digitalWrite (IN1, HIGH);
-    digitalWrite (IN2, LOW);
-    
-    digitalWrite (IN3, LOW);
-    digitalWrite (IN4, HIGH);
-    
-    delay(trta);
+  //Dobla al sentido contrario hacia delante
+  digitalWrite (IN1, HIGH);
+  digitalWrite (IN2, LOW);
+  
+  digitalWrite (IN3, LOW);
+  digitalWrite (IN4, HIGH);
+  
+  delay(trta);
 
 
-    digitalWrite (IN1, LOW);
-    digitalWrite (IN2, HIGH);
-    delayMicroseconds(tiempo_enderezar);
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, HIGH);
+  delayMicroseconds(tiempo_enderezar);
 
-    digitalWrite (IN1, LOW);
-    digitalWrite (IN2, LOW);
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, LOW);
 
-    delay(trdd);
+  delay(trdd);
 }
 
 
 
 void giro_izq(int trda,int trta,int trdd){
 
-    // motor delantero a un lado
-    analogWrite(ENA, velocidad_maxima);
-    digitalWrite (IN1,  HIGH);
-    digitalWrite (IN2, LOW);
-    
-    // motor trasero atras
-    analogWrite(ENB, velocidad_maxima);
-    digitalWrite (IN3, HIGH);
-    digitalWrite (IN4, LOW);
-    delay(1000);
+  // motor delantero a un lado
+  analogWrite(ENA, velocidad_maxima);
+  digitalWrite (IN1,  HIGH);
+  digitalWrite (IN2, LOW);
+  
+  // motor trasero atras
+  analogWrite(ENB, velocidad_maxima);
+  digitalWrite (IN3, HIGH);
+  digitalWrite (IN4, LOW);
+  delay(trda);
 
-    //Dobla al sentido contrario hacia delante
-    digitalWrite (IN1,  LOW);
-    digitalWrite (IN2, HIGH);
-    
-    digitalWrite (IN3, LOW);
-    digitalWrite (IN4, HIGH);
-    
-    delay(1000);
+  //Dobla al sentido contrario hacia delante
+  digitalWrite (IN1,  LOW);
+  digitalWrite (IN2, HIGH);
+  
+  digitalWrite (IN3, LOW);
+  digitalWrite (IN4, HIGH);
+  
+  delay(trta);
 
 
-    digitalWrite (IN1, HIGH);
-    digitalWrite (IN2, LOW);
-    delayMicroseconds(9000);
+  digitalWrite (IN1, HIGH);
+  digitalWrite (IN2, LOW);
+  delayMicroseconds(tiempo_enderezar);
 
-    digitalWrite (IN1, LOW);
-    digitalWrite (IN2, LOW);
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, LOW);
 
-    delay(1000);
+  delay(trdd);
 
 }
 
 void avanzar(){
   switch (var_estado_ruedas) {
-      case 0:
+    case 0:
 
-        var_estado_ruedas=1;
+      var_estado_ruedas=1;
 
+      digitalWrite (IN1, HIGH);
+      digitalWrite (IN2, LOW);
+      delayMicroseconds(9000);
 
-      break;
-    
-      case 1:
+    break;
 
-        var_estado_ruedas=1;
-          //esta recto
-      break;
+    case 1:
 
-      case 3:
+      var_estado_ruedas=1;
+      //esta recto
       
-        var_estado_ruedas=1;
-          //esta en la
-        break;
+    break;
 
-      default: 
-          // if nothing else matches, do the default
-          // default is optional
-      break;
+    case 2:
+
+      var_estado_ruedas=1;
+      //esta en la
+
+      digitalWrite (IN1, HIGH);
+      digitalWrite (IN2, LOW);
+      delayMicroseconds(9000);
+
+    break;
+
+    default: 
+      // if nothing else matches, do the default
+      // default is optional
+    break;
   }
 }
 
